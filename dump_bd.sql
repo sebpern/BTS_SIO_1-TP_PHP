f2eebc136ac766ea197034d9af74020899980977
sebpern <spernelle@gmail.com>
2023-03-02 21:28:38 +0100

    Nouveau script BD

--- dump_bd.sql
+++ dump_bd.sql
@@ -16,23 +16,43 @@
 
 
 -- Listage de la structure de la base pour tp_php_sio
+DROP DATABASE IF EXISTS `tp_php_sio`;
 CREATE DATABASE IF NOT EXISTS `tp_php_sio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tp_php_sio`;
 
 -- Listage de la structure de table tp_php_sio. atelier
+DROP TABLE IF EXISTS `atelier`;
 CREATE TABLE IF NOT EXISTS `atelier` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_atelier` int NOT NULL AUTO_INCREMENT,
   `titre` varchar(50) NOT NULL,
   `nb_inscrits` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `fk_categorie` int NOT NULL,
+  PRIMARY KEY (`id_atelier`) USING BTREE,
+  KEY `FK_atelier_categorie` (`fk_categorie`),
+  CONSTRAINT `FK_atelier_categorie` FOREIGN KEY (`fk_categorie`) REFERENCES `categorie` (`id_categorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Listage des données de la table tp_php_sio.atelier : ~2 rows (environ)
-INSERT INTO `atelier` (`id`, `titre`, `nb_inscrits`) VALUES
-	(1, 'Ateliers 1 ', 8),
-	(2, 'Ateliers 2', 0);
+INSERT INTO `atelier` (`id_atelier`, `titre`, `nb_inscrits`, `fk_categorie`) VALUES
+	(20, 'Don de moelle osseuse', 0, 1),
+	(22, 'Activité physique', 0, 5);
+
+-- Listage de la structure de table tp_php_sio. categorie
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id_categorie` int NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `code` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id_categorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table tp_php_sio.categorie : ~0 rows (environ)
+INSERT INTO `categorie` (`id_categorie`, `libelle`, `code`) VALUES
+	(1, 'Santé', 'SA'),
+	(5, 'Sport', 'SP');
 
 -- Listage de la structure de table tp_php_sio. user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `login` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -40,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table tp_php_sio.user : ~1 rows (environ)
+-- Listage des données de la table tp_php_sio.user : ~0 rows (environ)
 INSERT INTO `user` (`id`, `login`, `password`) VALUES
 	(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
 
